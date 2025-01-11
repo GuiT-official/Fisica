2dff32dabe84002c73efab7c057fe1d1e25a615d
GuiT-official <senhor32@outlook.com.br>
2025-01-11 15:59:27 -0300

    a

--- P1/grafico_dados_A.py
+++ P1/grafico_dados_A.py
@@ -3,7 +3,9 @@ import numpy as np
 
 D = {1510:5.0000,1500:4.9707,1200:3.9687,900:2.9668,600:1.9795,300:0.9824}
 err = [np.sqrt((0.05*1500/np.sqrt(5))**2+1/4)]+list(map(lambda x: 0.05*x/np.sqrt(x/300),list(D.keys())[1:]))
-plt.errorbar(D.keys(),D.values(),yerr=[4.9e-3]*len(D),xerr=[0.05*1500/np.sqrt(5)+1/4]+list(map(lambda x: 0.05*x/np.sqrt(x/300),list(D.keys())[1:])),fmt='o',markersize=6,capsize=5,ecolor='green')
+plt.errorbar(D.keys(),D.values(),yerr=[4.9e-3]*len(D),xerr=[0.05*1500/np.sqrt(5)+1/4]+
+             list(map(lambda x: 0.05*x/np.sqrt(x/300),list(D.keys())[1:])),
+             fmt='o',markersize=6,capsize=5,ecolor='green')
 
 X = np.linspace(0,1510)
 f = lambda x, xer, yer: (D[1500]+yer)/(1500+xer)*x
